390236ab32e25499b38522d0c3aaa66adaa3848c
Sirajus Salekin Prodhan <salekin01@outlook.com>
2021-12-22 01:53:54 +0100

    added setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "gc-connect.py",
+    version = "0.1",
+    py_modules = ['gc-connect'],
+    url = "https://github.com/salekin01/gc-connect",
+    author = "salekin",
+    install_requires = ["pyngrok"]
+)
