a69d5e30143c4ba134e993c6727f8f62e0d64821
Sirajus Salekin Prodhan <salekin01@outlook.com>
2021-12-22 01:57:55 +0100

    rename module name from gc-connect to gcconnect

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name = "gc-connect.py",
     version = "0.1",
-    py_modules = ['gc-connect'],
+    py_modules = ['gcconnect'],
     url = "https://github.com/salekin01/gc-connect",
     author = "salekin",
     install_requires = ["pyngrok"]
